705fc9e8210c74903d2820c1ec9cfa2f38136a87
Theanh <Theanh_v99@yahoo.de>
2025-05-04 16:48:51 +0200

    missing distance matching

--- imputation/Utils.py
+++ imputation/Utils.py
@@ -1,33 +1,44 @@
-import numpy as np
-import scipy
-import math
+from sklearn.neighbors import KDTree
+import random
 
-#HMI: howManyImputations
-#A Two-stage Calculation Using a Quadratic Rule (https://pubmed.ncbi.nlm.nih.gov/39211325/)
-#https://github.com/josherrickson/howManyImputations/blob/main/R/how_many_imputations.R
-#fmi = fraction of missing information
-#cv = Desired precision of standard errors. Default to .05. If the data
-#       were re-imputed, the estimated standard errors would differ by no more than this amount.
-#alpha = Significance level for choice of "conservative" FMI.
-#return = The number of required imputations to obtain the cv level of precision.
+def matcherid(d, t, matcher = "NN", k = 10, radius = 3):
+    """
+    Return vector of n0 positions in d.
+    :param d: Numeric vector with values from observed cases. np.array
+    :param t: Numeric vector with values from missing cases. np.array
+    :param matcher: matching method: n closest neighbors, every donor based on threshold distance, distance aided
+    :param donors: k donors (for n closest neighbors)
+    :return:
 
-def HMI(model, cv = 0.05, alpha = 0.05):
-    #pass model correctly
-    model = pool(model)
-    fmi = max(fmi)
-    z = qnorm(1-alpha/2)
-    fmiu = cdf(ppf(fmi) + z*sqrt(2/model))
-    math.ceil(1 + 1 / 2 * (fmiu / cv) ^ 2)
+    Example:
+    d = np.array([-5, 6, 0, 10, 12])
+    t = np.array([-6])
+    matcherid(d, t, matcher = "NN", k = 3)
+    matcherid(d, t, matcher="fixedNN", radius = 5)
+    """
+    if matcher == "NN": #random from n closest Donors
+        idx = []
+        tree = KDTree(d.reshape(-1, 1), leaf_size = 40)
+        #returns index k NN indices choose 1 on random
+        dist, ind = tree.query(t.reshape(-1, 1), k = k)
+        for list in ind:
+            idx.append(random.choice(list))
+        #returns indices of one random nearest neighbor for each t
+        return idx
+    elif matcher == "fixedNN": #fixed radius nearest neighbour
+        idx = []
+        tree = KDTree(d.reshape(-1, 1), leaf_size = 40)
+        #returns index k NN indices choose 1 on random
+        ind = tree.query_radius(t.reshape(-1, 1), radius)
+        for list in ind:
+            idx.append(random.choice(list))
+        #returns indices of one random nearest neighbor
+        return idx
+    # elif matcher == "distance": #based on distance of donor to imputed value (midas)
+    # matcher
+    #
+    #
+    else:
+        raise ValueError("unknown matcher")
 
-#R mice::pool
-#dfcom: A positive number representing the degrees of freedom in the complete-data analysis.
-#rule:  A string indicating the pooling rule
-def pool(object, dfcom = NULL, rule = NULL):
-    #...
-
-def pool_syn(object, dfcom = NULL):
-    #combines estimates by Reiter's partially synthetic data pooling rules
-    #This combination rule assumes that the data that is synthesised is completely observed
-
-#!!! Need utils function to get dfcom + cov + vcov
 
